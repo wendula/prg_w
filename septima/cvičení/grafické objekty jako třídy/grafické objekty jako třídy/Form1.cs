ba36d87c747699f9e27f5229a9d64fec4d38bb03
wendula <144894398+wendula@users.noreply.github.com>
2024-09-13 10:59:51 +0200

    uklizeni

---
+++ septima/cvičení/grafické objekty jako třídy/grafické objekty jako třídy/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace grafické_objekty_jako_třídy
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rectangle;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rectangle = new Rectangle(100, 100, 100, 100, Brushes.SkyBlue, Pens.BlueViolet, false);
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            rectangle.Draw(e.Graphics);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 'f')
+            {
+                rectangle.isFilled = false;
+                Refresh();
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                rectangle.Reposition(e.X, e.Y);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                Refresh();
+            }
+        }
+    }
+}
---
+++ septima/cvičení/grafické objekty jako třídy/grafické objekty jako třídy/rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafické_objekty_jako_třídy
+{
+    
+    internal class Rectangle
+    {
+        public int width, height, x, y;
+        public Brush brush;
+        public Pen pen;
+        public bool isFilled;
+
+        public Rectangle(int width, int height, int x, int y, Brush brush, Pen pen, bool isFilled)
+        {
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+            this.brush = brush;
+            this.pen = pen;
+            this.isFilled = isFilled;
+        }
+        public void Draw(Graphics g)
+        {
+            if (isFilled)
+            {
+                g.FillRectangle(brush, x, y, width, height);
+            }
+            else
+            {
+                g.DrawRectangle(pen, x, y, width, height);
+            }
+        }
+        public void Reposition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Resize(int x, int y)
+        {
+            width = Math.Abs(x - this.x);
+            height = Math.Abs(y - this.y);
+        }
+    }
+}
